f61cd89e8235ee0922847533bb287648ca3a1791
sawa-D <keisyuunivers102@gmail.com>
2025-05-26 00:34:25 +0900

    クラスに分解　インスタンス化し呼び出す内部処理を変更 動作自体には変更なし

---
+++ player.js
@@ -0,0 +1,62 @@
+/*
+playerクラス
+*/
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 40;
+        this.height = 40;
+        this.vx = 0;
+        this.vy = 0;
+        this.speed = 5;
+        this.jumpPower = 15;
+        this.onGround = false;
+    }
+
+    update(keys, platforms) {
+        // 左右移動
+        if (keys['KeyD']) this.vx = this.speed;
+        else if (keys['KeyA']) this.vx = -this.speed;
+        else this.vx = 0;
+
+        // ジャンプ
+        if (keys['Space'] && this.onGround) {
+            this.vy = -this.jumpPower;
+            this.onGround = false;
+        }
+
+        // 重力
+        this.vy += 0.7;
+
+        // 移動
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // 地面・障害物との当たり判定
+        this.onGround = false;
+        for (const p of platforms) {
+            // 簡易AABB判定
+            if (
+                this.x < p.x + p.width &&
+                this.x + this.width > p.x &&
+                this.y < p.y + p.height &&
+                this.y + this.height > p.y
+            ) {
+                // プレイヤーが上から着地
+                if (this.vy > 0 && this.y + this.height - this.vy <= p.y) {
+                    this.y = p.y - this.height;
+                    this.vy = 0;
+                    this.onGround = true;
+                }
+            }
+        }
+    }
+
+    draw(ctx, cameraX) {
+        ctx.fillStyle = '#ffb703';
+        ctx.fillRect(this.x - cameraX, this.y, this.width, this.height);
+    }
+}
+export default Player;
