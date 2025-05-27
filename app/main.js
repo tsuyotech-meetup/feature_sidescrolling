7d0e596eea67ea414d557e2d3b9101373140b16d
goegoe0212 <goegoe0212@outlook.jp>
2025-05-27 11:05:10 +0000

    fix: socket.ioの接続と切断イベントのログを追加

--- app/main.js
+++ app/main.js
@@ -10,6 +10,15 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+
+});
+
 server.listen(PORT, function () {
     console.log('server listening. Port:' + PORT);
 });
